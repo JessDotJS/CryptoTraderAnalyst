4de4c02b3de8e7e13003e1d104f2cf110abf4f33
Lesther Caballero <lesther993@gmail.com>
2017-06-07 16:01:46 -0400

    envio de datos de trades de btc_eth a firebase

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ var firebase = require("firebase");
 var config = require("./serviceAccountKey"); //Firebase config data
 
 firebase.initializeApp(config); //Initializing firebase
-var rootRef = firebase.database().ref();
+//var rootRef = firebase.database().ref();
+var orderBookRef = firebase.database().ref("poloniex/orderbook");
+
+var cryptoCurrency = ['BTC_XRP','BTC_ETH','BTC_FCT','BTC_XMR','BTC_STR','BTC_MAID','BTC_LTC','BTC_DOGE','BTC_DASH','BTC_BTS','BTC_CLAM'];
 
 var wsuri = "wss://api.poloniex.com";
 
@@ -17,6 +20,8 @@ var connection = new autobahn.Connection({
 connection.onopen = function (session) {
     function marketEvent (args,kwargs) {
         console.log(args);
+
+        sendData(args,cryptoCurrency[1]);
     }/*
     function tickerEvent (args,kwargs) {
         console.log(args);
@@ -24,7 +29,7 @@ connection.onopen = function (session) {
     /*function trollboxEvent (args,kwargs) {
         console.log(args);
     }*/
-    session.subscribe('BTC_ETH', marketEvent);
+    session.subscribe(cryptoCurrency[1], marketEvent);
     /*session.subscribe('ticker', tickerEvent);*/
     //session.subscribe('trollbox', trollboxEvent);
 }
@@ -42,28 +47,47 @@ connection.open();
 
 
 // DATA DE EJEMPLO
+var ee =[ { type: 'orderBookModify',
+    data: { type: 'ask', rate: '0.09000012', amount: '0.88260028' } },
+  { type: 'newTrade',
+    data:
+     { amount: '6.83090558',
+       date: '2017-06-06 12:24:17',
+       rate: '0.09000012',
+       total: '0.61478232',
+       tradeID: '26659710',
+       type: 'buy' } } ];
 
-// var ee = [ { type: 'orderBookModify',
-//     data: { type: 'ask', rate: '0.09081001', amount: '0.00553905' } 
-//    },
-//   { type: 'orderBookModify',
-//     data: { type: 'ask', rate: '0.09070999', amount: '10.94899736' } } ]
 
-var sendData = function(data){ //Func que enviara data a firebase
-    var temp1;
-    for (var i = 0 ; i < data.length; i++) {
-        temp1 = data[i];
-        if (temp1.type == 'orderBookModify') {
+
+
+var sendData = function(data_p,currency){ //Func que enviara data a firebase
+    var temp;
+    for (var i = 0 ; i < data_p.length; i++) {
+        temp = data_p[i];
+        var data = temp.data;
+        switch(temp.type){
+          case 'orderBookModify':
             console.log ("modif");
-        } else if(temp1.type == 'orderBookRemove'){
+            // rootRef.push({ name:"Lesther", email: "eel@gmail.com" });
+            break;
+          case 'orderBookRemove':
             console.log("remove");
-        } else if(temp1.type == "newTrade"){
-            console.log("trade");
-        }else{
-            console.log("error");
+            break;
+          case 'newTrade':
+            console.log("Trade");
+            //console.log({ amount:data.amount, date:data.date,total:data.total,tradeID:data.tradeID, type: data.type });
+            orderBookRef.child("trades"+"/"+currency).push({ amount:data.amount, date:data.date,total:data.total,tradeID:data.tradeID, type: data.type });
+            break;
+          default:
+            console.log("ERROR");
         }
-
     }
 }
 
 //sendData(ee);
+
+
+
+
+
