5cc6bd6d07b501710d2f26d9a41436f4ed822ae1
Lesther Caballero <lesther993@gmail.com>
2017-06-07 14:13:40 -0400

    added firebase setup

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var autobahn = require('autobahn');
+var firebase = require("firebase");
+var config = require("./serviceAccountKey"); //Firebase config data
+
+firebase.initializeApp(config); //Initializing firebase
+var rootRef = firebase.database().ref();
 
 var wsuri = "wss://api.poloniex.com";
 
@@ -10,18 +15,18 @@ var connection = new autobahn.Connection({
 
 
 connection.onopen = function (session) {
-    /*function marketEvent (args,kwargs) {
+    function marketEvent (args,kwargs) {
         console.log(args);
-    }
+    }/*
     function tickerEvent (args,kwargs) {
         console.log(args);
     }*/
-    function trollboxEvent (args,kwargs) {
+    /*function trollboxEvent (args,kwargs) {
         console.log(args);
-    }
-    /*session.subscribe('BTC_ETH', marketEvent);
-    session.subscribe('ticker', tickerEvent);*/
-    session.subscribe('trollbox', trollboxEvent);
+    }*/
+    session.subscribe('BTC_ETH', marketEvent);
+    /*session.subscribe('ticker', tickerEvent);*/
+    //session.subscribe('trollbox', trollboxEvent);
 }
 
 
@@ -33,3 +38,32 @@ connection.onclose = function () {
 
 
 connection.open();
+
+
+
+// DATA DE EJEMPLO
+
+// var ee = [ { type: 'orderBookModify',
+//     data: { type: 'ask', rate: '0.09081001', amount: '0.00553905' } 
+//    },
+//   { type: 'orderBookModify',
+//     data: { type: 'ask', rate: '0.09070999', amount: '10.94899736' } } ]
+
+var sendData = function(data){ //Func que enviara data a firebase
+    var temp1;
+    for (var i = 0 ; i < data.length; i++) {
+        temp1 = data[i];
+        if (temp1.type == 'orderBookModify') {
+            console.log ("modif");
+        } else if(temp1.type == 'orderBookRemove'){
+            console.log("remove");
+        } else if(temp1.type == "newTrade"){
+            console.log("trade");
+        }else{
+            console.log("error");
+        }
+
+    }
+}
+
+//sendData(ee);
---
+++ serviceAccountKey.js
@@ -0,0 +1,13 @@
+  var config = {
+    apiKey: "AIzaSyCtZ3pSfUlb-jdCJ_aN3J-FVszq3Ks9-pc",
+    authDomain: "cripto-example.firebaseapp.com",
+    databaseURL: "https://cripto-example.firebaseio.com",
+    projectId: "cripto-example",
+    storageBucket: "cripto-example.appspot.com",
+    messagingSenderId: "313811207356"
+  };
+
+  module.exports=config;
+
+  //CHANGE
+  // Firebase config data with my gmail account for testing
